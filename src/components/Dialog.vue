87298164e7245180413000f8551f4db62d33776b
Nicolas V <nicolas.valla20@gmail.com>
2024-08-04 21:05:15 -0400

    coding test

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -5,7 +5,7 @@
           <v-card-title class="mt-4 font-weight-bold">
             Article Details
           </v-card-title>
-          <v-card-text v-if="article && article.story_title && article.author && article.comment_text" class="overflow-auto" style="max-height:50vh; overflow-x: auto;">
+          <v-card-text v-if="article && article.story_title && article.author && article.comment_text" class="overflow-auto" style="max-height:50vh">
             <p class="mt-4 mb-2"><strong>Title:</strong> {{ article.story_title }}</p>
             <p class="mb-2"><strong>Author:</strong> {{ article.author }}</p>
             <p class="mb-2"><strong>Comments:</strong> {{ cleanCommentText(article.comment_text) }}</p>
