abe0306ef3e4a2a8f4129c3d05a6d841cab09d18
Nicolas V <nicolas.valla20@gmail.com>
2024-08-15 20:22:13 -0400

    style: apply formatting

--- src/services/getData.js
+++ src/services/getData.js
@@ -1,16 +1,16 @@
-const API_KEY = import.meta.env.VITE_ARTICLES_API_KEY;
+const API_KEY = import.meta.env.VITE_ARTICLES_API_KEY
 
 export const getData = async () => {
   try {
-    const response = await fetch(API_KEY);
-    const data = await response.json();
+    const response = await fetch(API_KEY)
+    const data = await response.json()
     const sortData = data.hits.sort((a, b) => {
-      const creation = new Date(a.created_at);
-      const creation2 = new Date(b.created_at);
-      return creation2 - creation;
-    });
-    return sortData;
+      const creation = new Date(a.created_at)
+      const creation2 = new Date(b.created_at)
+      return creation2 - creation
+    })
+    return sortData
   } catch (error) {
-    console.log("Server Fetching Error", error);
+    console.log('Server Fetching Error', error)
   }
-};
+}
